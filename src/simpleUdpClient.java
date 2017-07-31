85bfb8956da45616053bb4e3371ae110c737b3db
rarudo <rarudonet@gmail.com>
2017-07-31 13:04:51 +0900

    印刷用に微修正

--- src/simpleUdpClient.java
+++ src/simpleUdpClient.java
@@ -9,19 +9,15 @@ import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.util.Random;
 
-/**
- * Created by user on 2017/07/21.
- */
-
 class simpleUdpClient{
     public static void main(String args[]) throws InterruptedException {
         sendPacket("メッセージ!!!","FF00000");
     }
 
     //sendPacket("送信するメッセージ","色(16進数表記)")
-    //sendPacket("黒色のめっせーじ","000000") 黒色
-    //sendPacket("てすとめっせーじ","FFFFFF") 白色
-    //sendPacket("てすとめっせーじ","FF0000") 赤色
+    //sendPacket("黒色のメッセージ","000000") 黒色
+    //sendPacket("てすとメッセージ","FFFFFF") 白色
+    //sendPacket("てすとメッセージ","FF0000") 赤色
     public static void sendPacket(String message,String colorCode){
         //16進数で色を表す
         String sendMsg= colorCode + message;
@@ -30,7 +26,7 @@ class simpleUdpClient{
             int port = 8800;
             DatagramSocket socket = new DatagramSocket(); // ソケットの作成
             // 送信データ用 DatagramPacket の作成
-            InetAddress serverAddress = InetAddress.getByName("10.180.100.219");
+            InetAddress serverAddress = InetAddress.getByName("10.180.100.222");
             DatagramPacket sendPacket = new DatagramPacket(request,
                     request.length, serverAddress, port);
             socket.setSoTimeout(3000); // タイムアウトの設定 (3 秒)
